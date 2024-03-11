34b4b8721a37175edd9c1e7e8367bf7492b81235
latenightnourishment <sabine_mapa@dlsu.edu.ph>
2024-03-11 20:04:51 +0800

    installed handlebars

--- final_submission/app.js
+++ final_submission/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const path = require('path');
 
 const app = express();
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
 
 app.listen(8003);
 
 
 app.get('/', function(req,res) {
     res.redirect('/home.html');
-})
+});
 
 app.get('/home.html', function(req,res) {
     res.sendFile("/home.html");
-})
+});
+
+app.post('/home.html', function(req,res) {
+    res.send('debug');
+    console.log('debug');
+});
