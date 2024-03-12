1decee6f75ef9dbee00749d0005900a58f133edf
Francisco Emmanuel Dumas <francisco_dumas@dlsu.edu.ph>
2024-03-13 00:09:13 +0800

    Fixed Profile

--- final_submission/src/routers/profiles.js
+++ final_submission/src/routers/profiles.js
@@ -7,9 +7,10 @@ const db = getDb();
 const profile = db.collection('profiles');
 
 profileRouter.get('/profiles/:username', async (req,res) => {
-    var user = req.params.username
+    var user = req.params.username;
+    console.log(user);
     const profiles = await profile.find({"username":user}).toArray();
-    if (profile.length > 0){
+    if (profiles.length > 0){
         const profileobject = {"username":user,"name":profiles[0].name,"bio":profiles[0].bio}
         res.render("profile", {
             title: "Profile",
