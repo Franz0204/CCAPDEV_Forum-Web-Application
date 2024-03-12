3daf80f6d9f1d88fba99e35741345da528dcb270
Francisco Emmanuel Dumas <francisco_dumas@dlsu.edu.ph>
2024-03-13 00:00:00 +0800

    Added Profile.hbs

--- final_submission/src/routers/index.js
+++ final_submission/src/routers/index.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-//import profileRouter from './profiles.js';
+import profileRouter from './profiles.js';
 import postRouter from './posts.js';
 //import loginRouter from './login.js';
 
 const router = Router();
 
 router.use(postRouter);
-
+router.use(profileRouter);
 
 router.get('/', function(req,res) {
     res.redirect('/home');
