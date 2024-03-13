959836749da76f20e10d07bef5dd83b9e7b84d81
Francisco Emmanuel Dumas <francisco_dumas@dlsu.edu.ph>
2024-03-13 16:27:09 +0800

    Update

--- final_submission/src/routers/register.js
+++ final_submission/src/routers/register.js
@@ -2,18 +2,17 @@ import { Router } from 'express';
 import { getDb } from '../db/conn.js';
 import { ObjectId } from 'mongodb';
 
-
 const registerRouter = Router();
 const db = getDb();
 const credentials = db.collection('credentials');
 
 
-registerRouter.get('/credentials/', async (req,res) => {
+registerRouter.get('/register', async (req,res) => {
 var user = req.params.username;
 const credentials = await credentials.find ({"username":user}).toArray();
     if (credentials.length > 0) {
         const registerobject = {"username":credentials[0].username,"email":credentials[0].email,"handle":credentials[0].handle,"password":credentials[0].password}
-        res.render("credentials", {
+        res.render("register", {
             title: "Credentials",
             username: registerobject.username,
             email: registerobject.email,
