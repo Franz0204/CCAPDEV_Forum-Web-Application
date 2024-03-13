7be11cfdc343895fdcaf8a85366e4204840c5c2a
Francisco Emmanuel Dumas <francisco_dumas@dlsu.edu.ph>
2024-03-13 16:50:04 +0800

    Added register router

--- final_submission/src/routers/index.js
+++ final_submission/src/routers/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import registerRouter from './register.js';
 import profileRouter from './profiles.js';
 import postRouter from './posts.js';
 //import loginRouter from './login.js';
 
 const router = Router();
 
+router.use(registerRouter);
 router.use(postRouter);
 router.use(profileRouter);
 
--- final_submission/src/routers/register.js
+++ final_submission/src/routers/register.js
@@ -6,23 +6,12 @@ const registerRouter = Router();
 const db = getDb();
 const credentials = db.collection('credentials');
 
-
 registerRouter.get('/register', async (req,res) => {
-var user = req.params.username;
-const credentials = await credentials.find ({"username":user}).toArray();
-    if (credentials.length > 0) {
-        const registerobject = {"username":credentials[0].username,"email":credentials[0].email,"handle":credentials[0].handle,"password":credentials[0].password}
-        res.render("register", {
-            title: "Credentials",
-            username: registerobject.username,
-            email: registerobject.email,
-            handle: registerobject.handle,
-            password: registerobject.password
-
-        });
-    
-    }
-
+    const credential = await credentials.find({}).toArray();
+    res.render("register", {
+        title: "Home",
+        posts: credential
+    });
 });
 
 export default registerRouter;
