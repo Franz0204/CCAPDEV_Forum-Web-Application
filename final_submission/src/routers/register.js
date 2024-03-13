83d9be0bf531f56f94e36eaa781970dd53b85f32
JUAN GABRIEL ALONZO <juan_gabriel_alonzo@dlsu.edu.ph>
2024-03-13 16:16:20 +0800

    initial register routing

---
+++ final_submission/src/routers/register.js
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { getDb } from '../db/conn.js';
+import { ObjectId } from 'mongodb';
+
+
+const registerRouter = Router();
+const db = getDb();
+const credentials = db.collection('credentials');
+
+
+registerRouter.get('/credentials/', async (req,res) => {
+var user = req.params.username;
+const credentials = await credentials.find ({"username":user}).toArray();
+    if (credentials.length > 0) {
+        const registerobject = {"username":credentials[0].username,"email":credentials[0].email,"handle":credentials[0].handle,"password":credentials[0].password}
+        res.render("credentials", {
+            title: "Credentials",
+            username: registerobject.username,
+            email: registerobject.email,
+            handle: registerobject.handle,
+            password: registerobject.password
+
+        });
+    
+    }
+
+});
+
+export default registerRouter;
