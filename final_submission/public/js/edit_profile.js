7890649961d1acdca0ef9ebae72593a9df938d20
Francisco Emmanuel Dumas <francisco_dumas@dlsu.edu.ph>
2024-03-15 17:51:59 +0800

    updated edit profile

--- final_submission/public/js/edit_profile.js
+++ final_submission/public/js/edit_profile.js
@@ -14,32 +14,40 @@ document.addEventListener("DOMContentLoaded", function () {
         e.preventDefault();
         const updatedUser = {
             username: document.getElementById('username').value,
+            password: document.getElementById('password').value,
             name: document.getElementById('name').value,
             bio: document.getElementById('about-me').value
         };
         console.log(updatedUser);
-        try {
-            const response = await fetch('/update-profile', {
-                method: 'PUT',
-                body: JSON.stringify(updatedUser),
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
+        if (validateField(updatedUser.username) && validateField(updatedUser.password) && validateField(updatedUser.name) && validateField(updatedUser.bio)) {
+            try {
+                const response = await fetch('/update-profile', {
+                    method: 'PUT',
+                    body: JSON.stringify(updatedUser),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                });
 
-            if (response.ok) {
-                console.log('Profile updated successfully');
-                window.location.href = '/profiles/' + updatedUser.username;
-            } else {
-                console.error('Failed to update profile');
+                if (response.ok) {
+                    console.log('Profile updated successfully');
+                    window.location.href = '/profiles/' + updatedUser.username;
+                } else {
+                    console.error('Failed to update profile');
+                }
+            } catch (error) {
+                console.error('Error updating profile:', error);
             }
-        } catch (error) {
-            console.error('Error updating profile:', error);
         }
     });
+    
 
     cancelButton?.addEventListener("click", function () {
         console.log("test");
         window.location.href = '/profiles/{{this.username}}';
     });
+
+    function validateField(value) {
+        return value.trim() !== "";
+    }
 });
